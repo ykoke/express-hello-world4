f832d63bb721ea35cccf66015f1dc36702372aa3
ykoke <166777088+ykoke@users.noreply.github.com>
2024-06-21 15:55:47 +0900

    20240621

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const expressWs = require('express-ws')
 
-app.get("/", (req, res) => res.type('html').send(html));
+const app = express()
+expressWs(app)
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const port = process.env.PORT || 3001
+let connects = []
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.use(express.static('public'))
+
+app.ws('/ws', (ws, req) => {
+  connects.push(ws)
+
+  ws.on('message', (message) => {
+    console.log('Received:', message)
+
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        // Check if the connection is open
+        socket.send(message)
+      }
+    })
+  })
+
+  ws.on('close', () => {
+    connects = connects.filter((conn) => conn !== ws)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
 
 const html = `
 <!DOCTYPE html>
